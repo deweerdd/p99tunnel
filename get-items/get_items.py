0db6cefd24549c8e42595c39dc382c32dee8d8d0
Project1999 Auctions <p99tunnel@gmail.com>
2017-01-01 21:55:36 -0800

    Export items to CSV

--- get-items/get_items.py
+++ get-items/get_items.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-import bs4
+import csv
+import os
 import urllib.request
+
+import bs4
+
 
 BASE_WIKI_URL = 'http://wiki.project1999.com'
 # For some reason, if we don't use pageuntil=A to get the first page, we miss
 # out on some stuff like '2nd Piece of Staff'
 WIKI_ITEMS_URL = BASE_WIKI_URL + '/index.php?title=Category:Items&pageuntil=A'
+OUTPUT_NAME = 'items.csv'
 
 
 class Item(object):
@@ -37,13 +42,25 @@ def parse_one_page(url_to_scrape):
   return items, next_url
 
 
+def export_to_csv(items):
+  print('Exporting to CSV')
+  directory = os.path.dirname(os.path.realpath(__file__))
+  output_path = os.path.join(directory, OUTPUT_NAME)
+  with open(output_path, 'w', newline='') as output_file:
+    csv_writer = csv.writer(output_file)
+    csv_writer.writerow(['name', 'wiki_link'])
+    for item in items:
+      csv_writer.writerow([item.name, item.wiki_link])
+
+
 def main():
   next_url = WIKI_ITEMS_URL
   items = []
   while next_url is not None:
     new_items, next_url = parse_one_page(next_url)
     items += new_items
-  print(len(items))
+  export_to_csv(items)
+  print('Exported {} items!'.format(len(items)))
 
 
 if __name__ == '__main__':
