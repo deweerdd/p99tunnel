e1d4873c146faf5a507fd93ed7bdbec31bfc1cb5
Project1999 Auctions <p99tunnel@gmail.com>
2017-01-02 17:41:39 -0800

    Update get_items to support spells

--- get_items/get_items.py
+++ get_items/get_items.py
@@ -11,6 +11,8 @@ BASE_WIKI_URL = 'http://wiki.project1999.com'
 # For some reason, if we don't use pageuntil=A to get the first page, we miss
 # out on some stuff like '2nd Piece of Staff'
 WIKI_ITEMS_URL = BASE_WIKI_URL + '/index.php?title=Category:Items&pageuntil=A'
+WIKI_SPELLS_URL = (
+    BASE_WIKI_URL + '/index.php?title=Category:Spells&pageuntil=Acumen')
 OUTPUT_NAME = 'items.csv'
 
 
@@ -26,7 +28,21 @@ class Item(object):
     return self.name
 
 
-def parse_one_page(url_to_scrape):
+def get_item(is_spell, href, title):
+  # Make sure we don't get the "next 200" or "previous 200" pages.
+  if 'index.php?' in href:
+    return None
+  # Spells have their own, separate, parsing step since most spells don't have
+  # item pages but some do.
+  if title.startswith('Spell: '):
+    return None
+  if is_spell:
+    return Item('Spell: ' + title, href)
+  else:
+    return Item(title, href)
+
+
+def parse_one_page(url_to_scrape, is_spell):
   print('Parsing Page: {}'.format(url_to_scrape))
   page = urllib.request.urlopen(url_to_scrape)
   scraper = bs4.BeautifulSoup(page)
@@ -35,10 +51,12 @@ def parse_one_page(url_to_scrape):
   next_url = None
   for link in links.find_all('a'):
     href = link.get('href')
-    if 'index.php?' not in href:
-      items.append(Item(link.get('title'), href))
+    title = link.get('title')
     if 'next 200' == link.string:
       next_url = BASE_WIKI_URL + href
+    item = get_item(is_spell, href, title)
+    if item:
+      items.append(item)
   return items, next_url
 
 
@@ -57,7 +75,11 @@ def main():
   next_url = WIKI_ITEMS_URL
   items = []
   while next_url is not None:
-    new_items, next_url = parse_one_page(next_url)
+    new_items, next_url = parse_one_page(next_url, is_spell=False)
+    items += new_items
+  next_url = WIKI_SPELLS_URL
+  while next_url is not None:
+    new_items, next_url = parse_one_page(next_url, is_spell=True)
     items += new_items
   export_to_csv(items)
   print('Exported {} items!'.format(len(items)))
