1470668c5b394aed6de9ceb78a188d72345238be
Project1999 Auctions <p99tunnel@gmail.com>
2017-01-02 17:24:30 -0800

    Add a note about PYTHONPATH

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ set -euxo pipefail
 sudo apt-get install -y python3 python3-pip
 sudo apt-get install -y postgresql postgresql-contrib postgresql-client
 sudo apt-get install -y python3-psycopg2
+
+echo NOTE: you'll need to add your p99tunnel directory to your PYTHONPATH.
